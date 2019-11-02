f35971e37e178d9c6fda276b52fa2aa29bc40604
Ehsan Majdabadi <Majdabadi@chargoon.net>
2019-11-02 15:12:19 +0330

    Fix cache key.

--- src/language.ts
+++ src/language.ts
@@ -2,14 +2,15 @@ class Cache {
 	static languageCache: { key: string; value: string }[] = [];
 
 	static find(key: string, softwareGuid: string) {
-		const cachedItem = Cache.languageCache.filter(x => x.key === key)[0];
+		const refinedKey = `${key}-${softwareGuid}`;
+		const cachedItem = Cache.languageCache.filter(x => x.key === refinedKey)[0];
 		if (!!cachedItem) {
 			return cachedItem.value;
 		}
 
 		const translatedValue = window['LanguageEX'].GetString(key, softwareGuid);
 		Cache.languageCache.push({
-			key,
+			key:refinedKey,
 			value: translatedValue
 		});
 		return translatedValue;
