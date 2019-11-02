5bd20285429f26297594126b8b95d27c8755855c
Ehsan Majdabadi <Majdabadi@chargoon.net>
2019-11-02 15:36:14 +0330

    build project

--- build/didgah-language.js
+++ build/didgah-language.js
@@ -46,32 +46,17 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
 /******/ 		}
 /******/ 	};
 /******/
 /******/ 	// define __esModule on exports
 /******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
 /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -110,13 +95,14 @@ var Cache = /** @class */ (function () {
     function Cache() {
     }
     Cache.find = function (key, softwareGuid) {
-        var cachedItem = Cache.languageCache.filter(function (x) { return x.key === key; })[0];
+        var refinedKey = key + "-" + softwareGuid;
+        var cachedItem = Cache.languageCache.filter(function (x) { return x.key === refinedKey; })[0];
         if (!!cachedItem) {
             return cachedItem.value;
         }
         var translatedValue = window['LanguageEX'].GetString(key, softwareGuid);
         Cache.languageCache.push({
-            key: key,
+            key: refinedKey,
             value: translatedValue
         });
         return translatedValue;
